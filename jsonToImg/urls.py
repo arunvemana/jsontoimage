91b802aa9629eb2fbcd099bb8cf98e72f348cf93
arun <arunsai4b6@gmail.com>
2021-05-01 23:16:58 +0530

    :tada:version 1

--- jsonToImg/urls.py
+++ jsonToImg/urls.py
@@ -17,8 +17,9 @@ from django.contrib import admin
 from django.urls import path
 from startapp import views
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.index),
+    path('', views.check),
 
 ]
